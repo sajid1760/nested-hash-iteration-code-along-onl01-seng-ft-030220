67ae089846be9586cb8c5dde0d2f220c454b751a
Sajid Anjum <sajid1760@gmail.com>
2020-03-10 22:25:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,10 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  #binding.pry
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute,data|
+      if attribute == :favorite_ice_cream_flavors
+    end
+    end 
 end
 end
